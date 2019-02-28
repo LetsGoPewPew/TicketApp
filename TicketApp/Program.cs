7af9537adc589a8efd205c8ab1e97ee971191af8
WeeDLY <jorgen.sater@gmail.com>
2019-02-28 14:27:54 +0100

    test

--- TicketApp/Program.cs
+++ TicketApp/Program.cs
@@ -14,6 +14,7 @@ namespace TicketApp
         [STAThread]
         static void Main()
         {
+            int asd;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
