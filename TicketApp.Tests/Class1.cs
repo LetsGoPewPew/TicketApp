777c47b2cda7e8c0e2ab9bf8bdcc65d7f7ee7ab6
LetsGoPewPew <stian.dahlstrom@hotmail.com>
2019-02-28 14:47:03 +0100

    Added new project for tests, Nunit and empty test.

---
+++ TicketApp.Tests/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketApp;
+using NUnit.Framework;
+
+namespace TicketApp.Tests
+{
+    [TestFixture]
+    public class TicketTests
+    {
+        [Test]
+        public void Aseert_that_id_is_one()
+        {
+            
+        }
+    }
+}
