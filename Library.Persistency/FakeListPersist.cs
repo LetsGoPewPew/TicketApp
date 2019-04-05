69619197daa08411941b7018bd48568d82ba9150
stian D <tricknshit@hotmail.com>
2019-04-05 11:34:29 +0200

    changed name of method, added a fake class for testing persistance

---
+++ Library.Persistency/FakeListPersist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Model;
+
+namespace Library.Persistency
+{
+    class FakeListPersist : IPersistency
+    {
+        public void Persist(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Persist(SocialEvent socialEvent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Persist(Organizer organizer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Persist(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Persist(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
