a1687b57ae874ee30a91c784e13fd655afd165a3
WeeDLY <jorgen.sater@gmail.com>
2019-04-05 11:43:03 +0200

    Added simple persistency "pseudocode"

---
+++ Library.Persistency/Persistent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistency
+{
+    public class Persistent<T>
+    {
+        private ObservableCollection<T> _List;
+        public ObservableCollection<T> List { get => _List; set => _List = value; }
+        
+        public Persistent()
+        {
+            List.CollectionChanged += List_CollectionChanged;
+        }
+
+        private void List_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            Debug.WriteLine("Change type: " + e.Action);
+            if (e.NewItems != null)
+            {
+                Debug.WriteLine("Items added: ");
+                foreach (var item in e.NewItems)
+                {
+                    Debug.WriteLine(item);
+                }
+            }
+
+            if (e.OldItems != null)
+            {
+                Debug.WriteLine("Items removed: ");
+                foreach (var item in e.OldItems)
+                {
+                    Debug.WriteLine(item);
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Library.Persistency/TestPersistent.cs
@@ -0,0 +1,14 @@
+﻿using Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistency
+{
+    class TestPersistent
+    {
+        Persistent<Customer> customers { get; set; }
+    }
+}
