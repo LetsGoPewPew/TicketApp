d495be6ba63c676ebc20144f3eb76a0b5ea1d84f
WeeDLY <jorgen.sater@gmail.com>
2019-04-05 11:07:10 +0200

    Added library for persistency. Added IPersistency with overloads for storing data

---
+++ Library.Persistency/IPersistency.cs
@@ -0,0 +1,18 @@
+﻿using Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistency
+{
+    public interface IPersistency
+    {
+        void Store(string data);
+        void Store(SocialEvent socialEvent);
+        void Store(Organizer organizer);
+        void Store(Customer customer);
+        void Store(Ticket ticket);
+    }
+}
