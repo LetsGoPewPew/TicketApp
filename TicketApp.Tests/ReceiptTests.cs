592fdd5e60616631a0a73ad66459c54a5180c38c
stian D <tricknshit@hotmail.com>
2019-04-08 13:41:07 +0200

    Added ApprovalTests nuget to Tests project. Added Class receipt Added ApprovalTest for receipt added *.received.* to gitignore to stop approvaltest to clutter sourcecontrol with files from the approvaltests

---
+++ TicketApp.Tests/ReceiptTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Model;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+
+namespace TicketApp.Tests
+{
+    [UseReporter(typeof(VisualStudioReporter))]
+    [TestFixture]
+    class ReceiptTests
+    {
+        [Test]
+        public void Assert_reciept_is_correct()
+        {
+            SocialEvent socialEvent = new SocialEvent(10, 100, "TestCategory", "TestName");
+            Ticket ticket = new Ticket(socialEvent);
+            Receipt receipt = new Receipt(4, ticket, "kr");
+            string result = receipt.GenerateReceipt();
+
+            Approvals.Verify(result);
+        }
+    }
+}
