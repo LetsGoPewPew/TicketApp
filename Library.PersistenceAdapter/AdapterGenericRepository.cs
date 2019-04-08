967385beee46c3257285ff5f801dbcd362f6dc40
WeeDLY <jorgen.sater@gmail.com>
2019-04-08 17:16:26 +0200

    AdapterGenericRepository can now add and remove entries in IRepository

--- Library.PersistenceAdapter/AdapterGenericRepository.cs
+++ Library.PersistenceAdapter/AdapterGenericRepository.cs
@@ -1,5 +1,8 @@
-﻿using Library.Persistency;
+﻿using Library.Model;
+using Library.Persistency;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Library.PersistenceAdapter
 {
@@ -14,12 +17,42 @@ namespace Library.PersistenceAdapter
 
         public void Add(T entity)
         {
-
+            switch (entity)
+            {
+                default:
+                case Customer customer:
+                    UnitOfWorkRepository.CustomerRepository.Add(entity as Customer);
+                    break;
+                case Organizer organizer:
+                    UnitOfWorkRepository.OrganizerRepository.Add(entity as Organizer);
+                    break;
+                case SocialEvent socialEvent:
+                    UnitOfWorkRepository.SocialEventRepository.Add(entity as SocialEvent);
+                    break;
+                case Ticket ticket:
+                    UnitOfWorkRepository.TicketRepository.Add(entity as Ticket);
+                    break;
+            }
         }
 
-        public void Remove(T a)
+        public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            switch (entity)
+            {
+                default:
+                case Customer customer:
+                    UnitOfWorkRepository.CustomerRepository.Remove(entity as Customer);
+                    break;
+                case Organizer organizer:
+                    UnitOfWorkRepository.OrganizerRepository.Remove(entity as Organizer);
+                    break;
+                case SocialEvent socialEvent:
+                    UnitOfWorkRepository.SocialEventRepository.Remove(entity as SocialEvent);
+                    break;
+                case Ticket ticket:
+                    UnitOfWorkRepository.TicketRepository.Remove(entity as Ticket);
+                    break;
+            }
         }
 
         public void Dispose()
