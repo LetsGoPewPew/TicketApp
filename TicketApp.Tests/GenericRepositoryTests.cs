8c066ed1cfb737206ef7dafbc6a903e1b54da8f6
WeeDLY <jorgen.sater@gmail.com>
2019-04-09 13:23:27 +0200

    Added GenericRepositoryTests to test adding and removing

---
+++ TicketApp.Tests/GenericRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using Library.Persistency;
+using Library.Model;
+using NUnit.Framework;
+using Library.DataAccess;
+
+namespace TicketApp.Tests
+{
+    [TestFixture]
+    class GenericRepositoryTests
+    {
+        private GenericRepository<Customer> Customers;
+        [SetUp]
+        public void SetupBeforeEachTest()
+        {
+            Customers = new GenericRepository<Customer>(new MyDbContext("a"));
+        }
+
+        [Test]
+        public void Assert_add_and_remove_to_generic_repository()
+        {
+            Customer testCustomer = new Customer("1", "1", "1");
+            Customers.Add(testCustomer);
+            Assert.IsNotNull(Customers);
+
+            Customers.Remove(testCustomer);
+            Assert.NotNull(Customers)
+        }
+    }
+}
