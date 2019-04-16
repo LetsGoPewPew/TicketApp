2a9cc38c26b6c36fba1a27ef38654f7dad741478
WeeDLY <jorgen.sater@gmail.com>
2019-04-16 19:40:56 +0200

    Added FileGenericRepository and FileUnitOfWork.

---
+++ Library.Persistency/FileGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Persistency
+{
+    public class FileGenericRepository<T> : IRepository<T> where T : class
+    {
+        public IQueryable<T> Entities => throw new NotImplementedException();
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Library.Persistency/FileUnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Model;
+
+namespace Library.Persistency
+{
+    public class FileUnitOfWork : IUnitOfWork
+    {
+        public IRepository<Customer> CustomerRepository => throw new NotImplementedException();
+
+        public IRepository<Organizer> OrganizerRepository => throw new NotImplementedException();
+
+        public IRepository<SocialEvent> SocialEventRepository => throw new NotImplementedException();
+
+        public IRepository<Ticket> TicketRepository => throw new NotImplementedException();
+
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RejectChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
